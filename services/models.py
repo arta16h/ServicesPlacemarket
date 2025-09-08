26a5abe74fed2552cba451bb9df02becb37ef165
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 03:06:01 +0330

    Chore: adding str

--- services/models.py
+++ services/models.py
@@ -3,9 +3,16 @@ from django.db import models
 class ServiceCategory(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class SubCategory(models.Model):
-    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE, related_name='subcategories')
     name = models.CharField(max_length=100)
     base_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     provider = models.ForeignKey("users.ProviderProfile", on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.category.name} - {self.name}"
+
