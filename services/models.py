7de6b1fe2b7d91602d7cfc99513404b17be10438
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 03:09:15 +0330

    Chore: ProviderService model

--- services/models.py
+++ services/models.py
@@ -6,13 +6,24 @@ class ServiceCategory(models.Model):
     def __str__(self):
         return self.name
 
+
 class SubCategory(models.Model):
     category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE, related_name='subcategories')
     name = models.CharField(max_length=100)
     base_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    provider = models.ForeignKey("users.ProviderProfile", on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.category.name} - {self.name}"
+    
+
+class ProviderService(models.Model):
+# Links a ProviderProfile to a specific subservice and stores provider price
+    provider = models.ForeignKey('users.ProviderProfile', on_delete=models.CASCADE, related_name='services')
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        return f"{self.provider.user.username}: {self.subcategory.name} - {self.price}"
 
