30508db4dcd4c5f9e66f93b6a441bdd8e536dcd4
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 02:26:42 +0330

    Chore: ProviderProfile model

--- users/models.py
+++ users/models.py
@@ -6,3 +6,13 @@ class User(AbstractUser):
     is_provider = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
+
+class ProviderProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    main_category = models.CharField(max_length=50, choices=[
+        ("cleaning", "Cleaning"),
+        ("repair", "Repair"),
+        ("beauty", "Beauty"),
+    ])
+    service_area = models.CharField(max_length=255)
+    document = models.FileField(upload_to="documents/", null=True, blank=True)
