e7bebfcb35c02c89b6707772939512076284cc92
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 03:28:22 +0330

    Chore: Wallet detail and Transaction list view

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Wallet, Transaction
+from .serializers import WalletSerializer, TransactionSerializer
 
-# Create your views here.
+
+class WalletDetailView(generics.RetrieveAPIView):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
+
+
+class TransactionListCreateView(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
