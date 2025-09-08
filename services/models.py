b766e1fde27029d7bdce9b3bcceaccb644ea11e3
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 02:27:39 +0330

    Chore: category and subcategory model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    provider = models.ForeignKey("users.ProviderProfile", on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
