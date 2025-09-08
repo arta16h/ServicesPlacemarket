d01fd792c56792c259bd19a3bcf91cbe8e4d8865
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 03:14:56 +0330

    Chore: service category, subcategory, provider service views

--- services/views.py
+++ services/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import ServiceCategory, SubCategory, ProviderService
+from .serializers import ServiceCategorySerializer, SubCategorySerializer, ProviderServiceSerializer
 
-# Create your views here.
+
+class ServiceCategoryViewSet(viewsets.ModelViewSet):
+    queryset = ServiceCategory.objects.all()
+    serializer_class = ServiceCategorySerializer
+
+
+class SubCategoryViewSet(viewsets.ModelViewSet):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+
+
+class ProviderServiceViewSet(viewsets.ModelViewSet):
+    queryset = ProviderService.objects.all()
+    serializer_class = ProviderServiceSerializer
