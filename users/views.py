e56ab0a431a57cbcf47ed601911ba7065125c0f7
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 02:57:26 +0330

    Chore: register user view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import User, ProviderProfile
+from .serializers import UserSerializer, ProviderProfileSerializer
 
-# Create your views here.
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
