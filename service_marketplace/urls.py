922e3c530377c4aefa346e25a57f304cb580cab7
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 02:48:31 +0330

    Chore: including all apps url

--- service_marketplace/urls.py
+++ service_marketplace/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/services/', include('services.urls')),
+    path('api/payments/', include('payments.urls')),
+    path('api/orders/', include('orders.urls')),
+    path('api/reviews/', include('reviews.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
