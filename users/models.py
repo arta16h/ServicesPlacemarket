a7ca9748e049ecc52b8d0e09f19d5cec5bc83fcb
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 02:25:39 +0330

    Chore: user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+    is_provider = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
