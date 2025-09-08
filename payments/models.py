c14b72cb9552d18da17020384a9b7fdb3e595973
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 03:19:41 +0330

    Chore: wallet model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Wallet(models.Model):
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE, related_name='wallet')
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"اعتبار کیف پول ({self.user.username}): {self.balance}"
