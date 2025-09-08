0a6428c5105b3243f87fc96e0f33da066a3b90fe
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 02:59:55 +0330

    Chore: verify provider function

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import User, ProviderProfile
 from .serializers import UserSerializer, ProviderProfileSerializer
+from rest_framework.decorators import api_view
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -11,3 +12,14 @@ class RegisterUserView(generics.CreateAPIView):
 class RegisterProviderView(generics.CreateAPIView):
     queryset = ProviderProfile.objects.all()
     serializer_class = ProviderProfileSerializer
+
+
+@api_view(['POST'])
+def verify_provider(request, pk):
+    try:
+        p = ProviderProfile.objects.get(pk=pk)
+    except ProviderProfile.DoesNotExist:
+        return Response({'detail': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+    p.verified = True
+    p.save()
+    return Response({'detail': 'verified'})
