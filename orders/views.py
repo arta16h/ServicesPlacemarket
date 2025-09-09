58557479922ef9c4ef5955adaed65c034610dcfa
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 12:42:19 +0330

    Feat: Create order view

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Order
-from .serializers import OrderSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Order, ProviderAvailability
+from .serializers import OrderSerializer, ProviderAvailabilitySerializer
+from services.models import ProviderService
 
-
-class OrderViewSet(viewsets.ModelViewSet):
+# مشتری سفارش ایجاد می‌کند
+class CreateOrderView(generics.CreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        service = ProviderService.objects.get(id=self.request.data["service"])
+        total_price = service.price + service.base_fee
+        serializer.save(customer=self.request.user, provider=service.provider, total_price=total_price)
+
+
