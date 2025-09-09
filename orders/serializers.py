5bc7b2a9843b1f20d129b3a6c40c8551ca92072e
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 12:40:40 +0330

    Feat: Provider availability serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, ProviderAvailability
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    customer_name = serializers.CharField(source="customer.username", read_only=True)
+    provider_name = serializers.CharField(source="provider.user.username", read_only=True)
+    service_name = serializers.CharField(source="service.subcategory.name", read_only=True)
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = [
+            "id", "customer", "customer_name", "provider", "provider_name",
+            "service", "service_name", "address", "datetime", "payment_method",
+            "status", "total_price", "created_at"
+        ]
+        read_only_fields = ["status", "total_price"]
+
+
+class ProviderAvailabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProviderAvailability
+        fields = ["id", "date", "start_time", "end_time"]
