2bdeb67f6077441adba82facc7521f5f4363a449
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 18:37:52 +0330

    Refactor: User and Provider model

--- users/models.py
+++ users/models.py
@@ -1,34 +1,36 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
-class User(AbstractUser):
-    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    is_customer = models.BooleanField(default=False)
-    is_provider = models.BooleanField(default=False)
-    default_address = models.TextField(null=True, blank=True)
 
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('provider', 'Provider'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.username} ({'provider' if self.is_provider else 'customer' if self.is_customer else 'user'})"
+        return f"{self.username} ({self.role})"
 
 
 
 
-class ProviderProfile(models.Model):
+class Provider(models.Model):
     CATEGORY_CHOICES = [
     ('cleaning', 'Cleaning'),
     ('repair', 'Repair'),
     ('beauty', 'Beauty'),
     ]
-    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
-    main_category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, null=True, blank=True)
-    service_area = models.CharField(max_length=255, blank=True, null=True)
-    document = models.FileField(upload_to='provider_docs/', blank=True, null=True)
-    verified = models.BooleanField(default=False)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='provider')
+    main_category = models.CharField(max_length=30, choices=CATEGORY_CHOICES)
+    region = models.CharField(max_length=255, blank=True)
+    approved = models.BooleanField(default=False)
+    documents = models.FileField(upload_to='provider_docs/', null=True, blank=True)
     total_successful_orders = models.PositiveIntegerField(default=0)
     cancel_count = models.PositiveIntegerField(default=0)
     late_count = models.PositiveIntegerField(default=0)
 
-
     def __str__(self):
-        return f"ProviderProfile: {self.user.username}"
+        return self.user.username
