a7f5e3302a54e0ac6fbc0c73d2643b7089fb9a7a
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 19:22:13 +0330

    Feat: adding address url

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('register-provider/', RegisterProviderView.as_view(), name='register-provider'),
-    path('verify-provider/<int:pk>/', verify_provider, name='verify-provider'),
+    path('addresses/', AddressListCreateView.as_view(), name='addresses'),
+    path('provider-profile/', ProviderProfileView.as_view(), name='provider-profile'),
 ]
