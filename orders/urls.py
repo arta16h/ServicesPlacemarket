34b61625a0b006365116c96e3b30178032f4ae70
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 13:03:23 +0330

    Refactor: orders urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet
-
-
-router = DefaultRouter()
-router.register(r'', OrderViewSet)
-
+from django.urls import path
+from .views import (
+    CreateOrderView, CustomerOrdersView, ProviderOrdersView,
+    UpdateOrderStatusView, ProviderAvailabilityView
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("create/", CreateOrderView.as_view(), name="create-order"),
+    path("my-orders/", CustomerOrdersView.as_view(), name="customer-orders"),
+    path("provider-orders/", ProviderOrdersView.as_view(), name="provider-orders"),
+    path("update-status/<int:pk>/", UpdateOrderStatusView.as_view(), name="update-order-status"),
+    path("availability/", ProviderAvailabilityView.as_view(), name="provider-availability"),
 ]
