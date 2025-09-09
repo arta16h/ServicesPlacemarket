5cb21d35198b855d751c6cf1dee3ddd4cc8d4eb5
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 12:27:58 +0330

    Refactor: Provider service serializer

--- services/serializers.py
+++ services/serializers.py
@@ -1,20 +1,22 @@
 from rest_framework import serializers
 from .models import ServiceCategory, SubCategory, ProviderService
 
-
 class ServiceCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceCategory
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategory
-        fields = ['id', 'category', 'name', 'base_price']
+        fields = ["id", "name", "category"]
 
 
 class ProviderServiceSerializer(serializers.ModelSerializer):
+    subcategory_name = serializers.CharField(source="subcategory.name", read_only=True)
+    category_name = serializers.CharField(source="subcategory.category.name", read_only=True)
+
     class Meta:
         model = ProviderService
-        fields = ['id', 'provider', 'subcategory', 'price', 'description']
+        fields = ["id", "subcategory", "subcategory_name", "category_name", "price", "base_fee"]
