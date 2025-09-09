0590a4d2ad4733c79fce86dd25ab0ae932b0c2c1
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 12:38:50 +0330

    Fix: status choices

--- orders/models.py
+++ orders/models.py
@@ -22,7 +22,7 @@ class Order(models.Model):
     address = models.CharField(max_length=255)
     datetime = models.DateTimeField()
     payment_method = models.CharField(max_length=10, choices=PAYMENT_CHOICES)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+    status = models.CharField(max_length=10, choices=STATUS, default="pending")
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
