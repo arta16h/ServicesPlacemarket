66584e7450aa3b09426e177775b63bcf8ed162e0
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 12:27:19 +0330

    Refactor: Providerservice model

--- services/models.py
+++ services/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from users.models import ProviderProfile
+
 
 class ServiceCategory(models.Model):
     name = models.CharField(max_length=100)
@@ -8,9 +10,8 @@ class ServiceCategory(models.Model):
 
 
 class SubCategory(models.Model):
-    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE, related_name='subcategories')
+    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE, related_name="subcategories")
     name = models.CharField(max_length=100)
-    base_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"{self.category.name} - {self.name}"
@@ -18,10 +19,10 @@ class SubCategory(models.Model):
 
 class ProviderService(models.Model):
 # Links a ProviderProfile to a specific subservice and stores provider price
-    provider = models.ForeignKey('users.ProviderProfile', on_delete=models.CASCADE, related_name='services')
-    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    provider = models.ForeignKey(ProviderProfile, on_delete=models.CASCADE, related_name="services")
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name="provider_services")
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(blank=True, null=True)
+    base_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0, help_text="مثلاً ایاب و ذهاب")
 
 
     def __str__(self):
