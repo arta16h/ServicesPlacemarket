5da8b4729959492f1acbfabcd226d23d99aa15df
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 14:09:12 +0330

    Perf: Create review view

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Review
 from .serializers import ReviewSerializer
+from orders.models import Order, Provider
+from django.db.models import Avg, Count
 
-
-class ReviewListCreateView(generics.ListCreateAPIView):
-    queryset = Review.objects.all()
+# ثبت نظر و امتیاز
+class CreateReviewView(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        order_id = self.request.data.get("order")
+        try:
+            order = Order.objects.get(id=order_id, customer=self.request.user, status="done")
+        except Order.DoesNotExist:
+            raise ValueError("Invalid order or not completed")
+
+        if hasattr(order, "review"):
+            raise ValueError("This order already has a review")
+
+        serializer.save(
+            customer=self.request.user,
+            provider=order.provider,
+            order=order
+        )
+
