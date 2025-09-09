cc6a843a78f1b60fadb09bcb7bd1fa36b9297098
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 15:48:18 +0330

    Feat: transaction url

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import WalletView, DepositWalletView, WithdrawWalletView, PayOrderView
+from .views import WalletView, DepositWalletView, WithdrawWalletView, PayOrderView, TransactionListView
 
 urlpatterns = [
     path("wallet/", WalletView.as_view(), name="wallet"),
+    path("transactions/", TransactionListView.as_view(), name="transactions"),
     path("deposit/", DepositWalletView.as_view(), name="deposit-wallet"),
     path("withdraw/", WithdrawWalletView.as_view(), name="withdraw-wallet"),
     path("pay/<int:pk>/", PayOrderView.as_view(), name="pay-order"),
