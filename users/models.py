a9186e2b070906486d98269809b3d460a5f04ab1
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 18:38:31 +0330

    Feat: Address model

--- users/models.py
+++ users/models.py
@@ -34,3 +34,16 @@ class Provider(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='addresses')
+    label = models.CharField(max_length=100, blank=True)
+    full_address = models.TextField()
+    place_id = models.CharField(max_length=255, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.label or self.full_address[:30]}"
