f8cb5a78cfdab23a696c2912a5065751d9702c8b
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 14:14:34 +0330

    Feat: Provider Profile view

--- users/views.py
+++ users/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Avg
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import User, ProviderProfile
 from .serializers import UserSerializer, ProviderProfileSerializer
 from rest_framework.decorators import api_view
+from reviews.models import Review
+
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class RegisterProviderView(generics.CreateAPIView):
     queryset = ProviderProfile.objects.all()
@@ -23,3 +27,20 @@ def verify_provider(request, pk):
     p.verified = True
     p.save()
     return Response({'detail': 'verified'})
+
+
+class ProviderProfileView(generics.RetrieveAPIView):
+    serializer_class = ProviderProfileSerializer
+    queryset = ProviderProfile.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        provider = self.get_object()
+        avg_rating = Review.objects.filter(provider=provider).aggregate(Avg("rating"))["rating__avg"] or 0
+        data = {
+            "id": provider.id,
+            "name": provider.user.username,
+            "service": provider.service.name,
+            "bio": provider.bio,
+            "rating": round(avg_rating, 2)
+        }
+        return Response(data)
