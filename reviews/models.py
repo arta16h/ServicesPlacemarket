e1d312ae3bd06ad83f391056ba394880b59777d7
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 03:42:28 +0330

    Chore: Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    order = models.OneToOneField('orders.Order', on_delete=models.CASCADE, related_name='review')
+    customer = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    provider = models.ForeignKey('users.ProviderProfile', on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review {self.id} - {self.provider.user.username} : {self.rating}"
