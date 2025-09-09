1e0e0b5d2dbaafd84e0b5e5e0d17279d9e127a5b
arta16h <sevda.hayati2015@gmail.com>
2025-09-09 12:29:03 +0330

    Refactor: services urls

--- services/urls.py
+++ services/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ServiceCategoryViewSet, SubCategoryViewSet, ProviderServiceViewSet
-
-
-router = DefaultRouter()
-router.register(r'categories', ServiceCategoryViewSet)
-router.register(r'subcategories', SubCategoryViewSet)
-router.register(r'provider-services', ProviderServiceViewSet)
-
+from django.urls import path
+from .views import (
+    ServiceCategoryListView,
+    SubCategoryListView,
+    ProviderServiceListCreateView,
+    SearchServiceView
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("categories/", ServiceCategoryListView.as_view(), name="categories"),
+    path("subcategories/", SubCategoryListView.as_view(), name="subcategories"),
+    path("my-services/", ProviderServiceListCreateView.as_view(), name="my-services"),
+    path("search/", SearchServiceView.as_view(), name="search-services"),
 ]
