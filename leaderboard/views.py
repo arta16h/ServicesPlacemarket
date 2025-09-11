3678421bd10e25419ee2468e5068c69afc3202f1
arta16h <sevda.hayati2015@gmail.com>
2025-09-12 02:32:25 +0330

    Feat: leaderboard view

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import LeaderboardEntry
+from django.utils.timezone import now
 
-# Create your views here.
+class LeaderboardView(APIView):
+    def get(self, request):
+        today = now()
+        leaderboard = LeaderboardEntry.objects.filter(month=today.month, year=today.year).order_by("rank")
+        data = [
+            {
+                "provider": entry.provider.username,
+                "rank": entry.rank,
+                "completed_orders": entry.completed_orders,
+                "avg_rating": entry.avg_rating,
+                "score": entry.score,
+            }
+            for entry in leaderboard
+        ]
+        return Response(data)
