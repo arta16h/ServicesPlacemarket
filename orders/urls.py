1f60a9a15dff539acc017ad87dd2516d2237601f
arta16h <sevda.hayati2015@gmail.com>
2025-09-11 04:23:53 +0330

    Feat: adding availability urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     CreateOrderView, CustomerOrdersView, ProviderOrdersView,
-    UpdateOrderStatusView, ProviderAvailabilityView
+    UpdateOrderStatusView, ProviderAvailabilityView, BookAvailabilityView, ProviderAvailabilityListView, 
 )
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path("my-orders/", CustomerOrdersView.as_view(), name="customer-orders"),
     path("provider-orders/", ProviderOrdersView.as_view(), name="provider-orders"),
     path("update-status/<int:pk>/", UpdateOrderStatusView.as_view(), name="update-order-status"),
-    path("availability/", ProviderAvailabilityView.as_view(), name="provider-availability"),
+    path("availability/create/", ProviderAvailabilityView.as_view(), name="create-availability"),
+    path('availability/<int:pk>/book/', BookAvailabilityView.as_view(), name='book-availability'),
+    path('availability/provider/<int:pk>/', ProviderAvailabilityListView.as_view(), name='provider-availability'),
 ]
