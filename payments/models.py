d195fcf75c6d5898ae1345ba4403f6b75d43863e
arta16h <sevda.hayati2015@gmail.com>
2025-09-11 23:49:07 +0330

    Feat: Payment model

--- payments/models.py
+++ payments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 from orders.models import Order
+from django.conf import settings
 
 
 class Wallet(models.Model):
@@ -28,3 +29,26 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"{self.wallet.user.username} - {self.type} - {self.amount}"
+
+
+class Payment(models.Model):
+    PAYMENT_METHODS = (
+        ('wallet', 'Wallet'),
+        ('cash', 'Cash'),
+    )
+    STATUS = (
+        ('pending', 'Pending'),
+        ('paid', 'Paid'),
+        ('failed', 'Failed'),
+    )
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="payments")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="payments")
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
+    status = models.CharField(max_length=20, choices=STATUS, default='pending')
+    step = models.PositiveSmallIntegerField(default=1)  # مرحله پرداخت: ۱ یا ۲
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment {self.id} for Order {self.order.id} - Step {self.step}"
