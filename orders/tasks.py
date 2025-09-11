ab9440cc1a176fc30259f9e37e1242bd19c1ced2
arta16h <sevda.hayati2015@gmail.com>
2025-09-12 00:14:09 +0330

    Feat: send order reminder

---
+++ orders/tasks.py
@@ -0,0 +1,18 @@
+from celery import shared_task
+from django.contrib.auth import get_user_model
+from services.models import Service
+from notifications.models import Notification
+
+User = get_user_model()
+
+@shared_task
+def send_order_reminder(user_id, service_id):
+    try:
+        user = User.objects.get(id=user_id)
+        service = Service.objects.get(id=service_id)
+        Notification.objects.create(
+            user=user,
+            message=f"می‌خواید دوباره سرویس {service.name} رو برای این ماه رزرو کنید؟"
+        )
+    except Exception as e:
+        print(f"Reminder error: {e}")
