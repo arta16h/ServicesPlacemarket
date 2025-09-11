8f97ef9ed8c547bc2bec98328eba9800668ac736
arta16h <sevda.hayati2015@gmail.com>
2025-09-11 04:26:17 +0330

    Feat: get commission rate

--- payments/utils.py
+++ payments/utils.py
@@ -6,4 +6,12 @@ from .models import Wallet, Transaction
 def quantize_amount(amount):
     return (Decimal(amount)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
 
+def get_commission_rate(provider):
+    total_done = provider.orders.filter(status='done').count()
+    if total_done < 10:
+        return Decimal('0.20')
+    elif total_done < 50:
+        return Decimal('0.15')
+    return Decimal('0.10')
+
 
