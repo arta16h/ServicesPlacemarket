891f7a7f2212b5b8ad1be15a3a72724499a9b93b
arta16h <sevda.hayati2015@gmail.com>
2025-09-12 02:35:53 +0330

    Feat: registering Leaderboard setting

--- leaderboard/admin.py
+++ leaderboard/admin.py
@@ -7,4 +7,12 @@ class LeaderboardEntryAdmin(admin.ModelAdmin):
     list_filter = ("month", "year")
     search_fields = ("provider__username",)
 
+@admin.register(LeaderboardSettings)
+class LeaderboardSettingsAdmin(admin.ModelAdmin):
+    list_display = ("weight_orders", "weight_ratings")
 
+    def has_add_permission(self, request):
+        # فقط یه تنظیمات وجود داشته باشه
+        if LeaderboardSettings.objects.exists():
+            return False
+        return True
