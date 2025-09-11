9c9840c56d177d78df7ad36203239eaf02d5c8d0
arta16h <sevda.hayati2015@gmail.com>
2025-09-12 02:34:45 +0330

    Feat: calculate monthly leaderboard

---
+++ leaderboard/tasks.py
@@ -0,0 +1,21 @@
+from celery import shared_task
+from django.utils.timezone import now
+from users.models import ProviderStats
+from leaderboard.models import LeaderboardEntry
+
+
+@shared_task
+def calculate_monthly_leaderboard():
+    # برترین‌ها را بگیر
+    top_providers = ProviderStats.objects.order_by("-final_score")[:10]
+
+    for idx, stats in enumerate(top_providers, start=1):
+        LeaderboardEntry.objects.create(
+            provider=stats.provider,
+            rank=idx,
+            completed_orders=stats.completed_orders,
+            avg_rating=stats.avg_rating,
+            score=stats.final_score,
+            month=now().month,
+            year=now().year
+        )
