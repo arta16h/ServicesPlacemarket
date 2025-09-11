f9b689f8dbedf8b528b90453d0be97707fdccb68
arta16h <sevda.hayati2015@gmail.com>
2025-09-11 23:51:03 +0330

    Feat: Wallet deposite request view

--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,12 @@
+import requests
+from django.conf import settings
+from django.http import HttpResponse
 from rest_framework import generics, status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Transaction
+from .utils import get_commission_rate
 from .serializers import WalletSerializer, TransactionSerializer
 from orders.models import Order
 
@@ -29,12 +33,36 @@ class DepositWalletView(APIView):
     def post(self, request):
         amount = float(request.data.get("amount", 0))
         if amount <= 0:
-            return Response({"error": "Invalid amount"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "مقدار نامعتبر"}, status=status.HTTP_400_BAD_REQUEST)
         wallet = request.user.wallet
         wallet.balance += amount
         wallet.save()
         Transaction.objects.create(wallet=wallet, type="deposit", amount=amount)
-        return Response({"message": "Wallet charged", "balance": wallet.balance})
+        return Response({"message": "کیف پول شارژ شد", "اعتبار": wallet.balance})
+
+# اتصال به زرین‌پال برای شارژ
+class WalletDepositRequestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        amount = int(request.data.get("amount", 0))
+        if amount <= 0:
+            return Response({"status": "failed", "reason": "amount must be > 0"}, status=400)
+
+        req_data = {
+            "merchant_id": settings.ZARINPAL_MERCHANT_ID,
+            "amount": amount,
+            "description": "شارژ کیف پول",
+            "callback_url": settings.ZARINPAL_CALLBACK_URL,
+        }
+        response = requests.post(settings.ZARINPAL_REQUEST_URL, json=req_data).json()
+
+        if response["data"]["code"] == 100:
+            authority = response["data"]["authority"]
+            payment_url = settings.ZARINPAL_STARTPAY_URL + authority
+            return Response({"status": "ok", "url": payment_url})
+        else:
+            return Response({"status": "failed", "errors": response["errors"]}, status=400)
 
 # برداشت از کیف پول (ویژه ارائه‌دهنده‌ها)
 class WithdrawWalletView(APIView):
@@ -44,11 +72,11 @@ class WithdrawWalletView(APIView):
         amount = float(request.data.get("amount", 0))
         wallet = request.user.wallet
         if amount <= 0 or amount > wallet.balance:
-            return Response({"error": "Invalid amount"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "مقدار نامعتبر"}, status=status.HTTP_400_BAD_REQUEST)
         wallet.balance -= amount
         wallet.save()
         Transaction.objects.create(wallet=wallet, type="withdraw", amount=amount)
-        return Response({"message": "Withdrawal successful", "balance": wallet.balance})
+        return Response({"message": "برداشت موفق", "balance": wallet.balance})
 
 # پرداخت سفارش
 class PayOrderView(APIView):
@@ -58,14 +86,14 @@ class PayOrderView(APIView):
         try:
             order = Order.objects.get(id=pk, customer=request.user)
         except Order.DoesNotExist:
-            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": "سفارش موردنظر پیدا نشد"}, status=status.HTTP_404_NOT_FOUND)
 
         if order.status != "accepted":
-            return Response({"error": "Order must be accepted before payment"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "بعد از تائید سفارش، میتوانید پرداخت کنید"}, status=status.HTTP_400_BAD_REQUEST)
 
         wallet = request.user.wallet
         if wallet.balance < order.total_price:
-            return Response({"error": "Insufficient balance"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "موجودی ناکافی"}, status=status.HTTP_400_BAD_REQUEST)
 
         # کسر از کیف پول مشتری
         wallet.balance -= order.total_price
@@ -73,7 +101,7 @@ class PayOrderView(APIView):
         Transaction.objects.create(wallet=wallet, order=order, type="payment", amount=order.total_price)
 
         # محاسبه کمیسیون
-        commission_rate = 0.20  # 20%
+        commission_rate = get_commission_rate(provider=order.provider.user)
         commission = order.total_price * commission_rate
         provider_amount = order.total_price - commission
 
@@ -90,4 +118,6 @@ class PayOrderView(APIView):
         order.status = "done"
         order.save()
 
-        return Response({"message": "Payment successful", "commission": commission, "provider_amount": provider_amount})
+        return Response({"message": "پرداخت موفق", "comission": commission, "provider_amount": provider_amount})
+    
+
