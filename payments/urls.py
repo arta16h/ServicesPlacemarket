6b1397b06d703f7ba1b25353cc3d15f0644ce259
arta16h <sevda.hayati2015@gmail.com>
2025-09-11 23:53:31 +0330

    Feat: callback and deposite urls

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import WalletView, DepositWalletView, WithdrawWalletView, PayOrderView, TransactionListView
+from .views import (WalletView, DepositWalletView, WithdrawWalletView, 
+        PayOrderView, TransactionListView, WalletDepositRequestView, WalletDepositCallbackView)
 
 urlpatterns = [
     path("wallet/", WalletView.as_view(), name="wallet"),
+    path("wallet/deposit/request/", WalletDepositRequestView.as_view(), name="wallet-deposit-request"),
+    path("callback/", WalletDepositCallbackView.as_view(), name="wallet_callback"),
     path("transactions/", TransactionListView.as_view(), name="transactions"),
     path("deposit/", DepositWalletView.as_view(), name="deposit-wallet"),
     path("withdraw/", WithdrawWalletView.as_view(), name="withdraw-wallet"),
