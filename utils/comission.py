9924c59baf4c2659d1feaed14442b5962b841e00
arta16h <sevda.hayati2015@gmail.com>
2025-09-11 04:19:01 +0330

    Deleting utils folder

--- utils/comission.py
+++
@@ -1,7 +0,0 @@
-def get_commission_rate(provider):
-    total_orders = provider.orders.filter(status="done").count()
-    if total_orders < 10:
-        return 0.20
-    elif total_orders < 50:
-        return 0.15
-    return 0.10
