2333c148d94fb7518e7c99f3fc88eebf2b4d0f80
arta16h <sevda.hayati2015@gmail.com>
2025-09-12 02:35:28 +0330

    Feat: registering Leaderboard entry

--- leaderboard/admin.py
+++ leaderboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import LeaderboardEntry, LeaderboardSettings
 
-# Register your models here.
+@admin.register(LeaderboardEntry)
+class LeaderboardEntryAdmin(admin.ModelAdmin):
+    list_display = ("provider", "rank", "score", "month", "year")
+    list_filter = ("month", "year")
+    search_fields = ("provider__username",)
+
+
