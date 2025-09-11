38c7210d54f2ecc7f30f3c759a0fabac80f03dca
arta16h <sevda.hayati2015@gmail.com>
2025-09-11 23:38:41 +0330

    Feat: Protocol type router

--- service_marketplace/asgi.py
+++ service_marketplace/asgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+import notifications.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service_marketplace.settings')
+django.setup()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            notifications.routing.websocket_urlpatterns
+        )
+    ),
+})
+
