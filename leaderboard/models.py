4f6f303d418710199e45056fdd97dff01134fefd
arta16h <sevda.hayati2015@gmail.com>
2025-09-12 02:15:50 +0330

    Feat: Leaderboard entry model

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class LeaderboardEntry(models.Model):
+    provider = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rank = models.PositiveIntegerField()
+    completed_orders = models.PositiveIntegerField()
+    avg_rating = models.FloatField()
+    score = models.FloatField() 
+    month = models.PositiveIntegerField()
+    year = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ("provider", "month", "year")
+
+    def __str__(self):
+       return f"{self.provider.username} - Rank {self.rank} - Score {self.score}"
+    
+
+
+
