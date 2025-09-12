cf8f34a6db4ec8036a9617eedc72922e01843686
arta16h <sevda.hayati2015@gmail.com>
2025-09-12 12:09:20 +0330

    Feat: get address from coords

---
+++ users/utils.py
@@ -0,0 +1,12 @@
+import requests
+from django.conf import settings
+
+def get_address_from_coords(lat, lng):
+    api_key = settings.GOOGLE_MAPS_API_KEY
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&key={api_key}"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["status"] == "OK":
+        return data["results"][0]["formatted_address"]
+    return None
