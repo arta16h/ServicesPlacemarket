caeb15813c0b7a612daaf4883cebe6b018e86dc9
arta16h <sevda.hayati2015@gmail.com>
2025-09-14 20:29:12 +0330

    Fix: changing the name of mark as read view

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import NotificationListView, UnreadNotificationListView, MarkAsReadView
+from .views import NotificationListView, UnreadNotificationListView, MarkNotificationReadView
 import logging
 logger = logging.getLogger(__name__)
 
@@ -7,5 +7,5 @@ logger = logging.getLogger(__name__)
 urlpatterns = [
     path('', NotificationListView.as_view(), name='notifications-list'),
     path('unread/', UnreadNotificationListView.as_view(), name='notifications-unread'),
-    path('<int:pk>/read/', MarkAsReadView.as_view(), name='notification-mark-read'),
+    path('<int:pk>/read/', MarkNotificationReadView.as_view(), name='notification-mark-read'),
 ]
