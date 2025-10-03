a64620a8876d18b0b6246f5cd4d09740df7bba6d
arta16h <sevda.hayati2015@gmail.com>
2025-10-03 20:45:38 +0330

    Refactor: allowed hosts

--- service_marketplace/settings.py
+++ service_marketplace/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+ALLOWED_HOSTS = ["servicesplacemarket.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
